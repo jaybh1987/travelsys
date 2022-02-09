fdd0eab862cddd96758fd3e1d61e7da0102aedf7
laitmatus <laitmatus@pop-os.localdomain>
2022-02-09 15:43:16 +0530

    inserting record got fail need to be solved.

--- routes/triprecords/triprecord.js
+++ routes/triprecords/triprecord.js
@@ -1,18 +1,38 @@
 var express = require('express'); 
-
 var router = express.Router();
-
+var pl = require('../../lib/database_utils/db')
 
 router.post('/', function(req, res, next) { 
 
 
-    console.log('request body : ------------------> ', req.body)
-    // let js = JSON.stringify({    
-    //     "name": "Jay", 
-    //     "lastName": "Bhavsar"
-    // })
+  /**
+   *  body hear is :
+   *    body:  {
+  car: 'Innova',
+  startDate: '2022-02-05',
+  endDate: '2022-02-12',
+  stTime: '06:31',
+  edTime: '17:25'
+}
+
+   */
+
+  let t = JSON.parse(req.body.body) 
+
+  console.log('t = ', t, t.car)
+
+    const text = 'INSERT INTO trip(carname, startdate, starttime,enddate,endtime) VALUES($1, $2, $3, $4, $5) RETURNING *'
+    // const values = ['t.car', 't.startDate', 't.endDate', t.stTime, t.edTime]  
+    const values = ['t.car', 't.startDate', 't.endDate', 't.stTime', 't.edTime']  
+
+    pl.query(text, values)
+    .then( r => { 
+      console.log(res.rows[0])
+    })
+    .catch( e => console.error(e))
+
 
     res.send({'name':'jay'})
 });
   
-  module.exports = router;
+module.exports = router;

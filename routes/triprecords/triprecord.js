5e98480743d5d5e1c7aba973d7cbacb93b7da493
laitmatus <laitmatus@pop-os.localdomain>
2022-02-09 20:42:28 +0530

    insert operation successfully completed.

--- routes/triprecords/triprecord.js
+++ routes/triprecords/triprecord.js
@@ -1,6 +1,6 @@
 var express = require('express'); 
 var router = express.Router();
-var pl = require('../../lib/database_utils/db')
+var clnt = require('../../lib/database_utils/db')
 
 router.post('/', function(req, res, next) { 
 
@@ -19,18 +19,31 @@ router.post('/', function(req, res, next) {
 
   let t = JSON.parse(req.body.body) 
 
-  console.log('t = ', t, t.car)
+  // console.log('t = ', t, t.car) 
 
-    const text = 'INSERT INTO trip(carname, startdate, starttime,enddate,endtime) VALUES($1, $2, $3, $4, $5) RETURNING *'
-    // const values = ['t.car', 't.startDate', 't.endDate', t.stTime, t.edTime]  
-    const values = ['t.car', 't.startDate', 't.endDate', 't.stTime', 't.edTime']  
+  // let text = 'INSERT INTO trip(carname, startdate, starttime,enddate,endtime) VALUES($1, $2, $3, $4, $5)'
+  //   // const values = ['t.car', 't.startDate', 't.endDate', t.stTime, t.edTime]  
+  // let values = ['car', '20-12-2020', 't.stTime','20-12-2020', 't.edTime']
 
-    pl.query(text, values)
-    .then( r => { 
-      console.log(res.rows[0])
+  // console.log('text = ', text)
+
+  //   pl.query(text, values)
+  //   .then( r => { 
+  //     console.log(res.rows[0])
+  //   })
+  //   .catch( e => console.error(e))
+
+
+
+    let text = 'INSERT INTO trip(car, start_date, end_date, start_time, end_time) VALUES($1, $2, $3, $4, $5)'
+    let values = [t.car, t.startDate, t.endDate, t.stTime, t.edTime]
+
+    clnt
+    .query(text, values)
+    .then( res => {
+      console.log('res = ', res)  
     })
-    .catch( e => console.error(e))
-
+    .catch( e => console.log(e.stack))
 
     res.send({'name':'jay'})
 });
